457740ca38cdb4d37abfd215956ea9284d478c14
Lorenzo Giaccari <lollo.jakk99@me.com>
2022-12-30 16:15:48 +0100

    Successfully imported ML model in app and created predict method, solved issues #9 and #10

--- PythonCode/MachineLearning.py
+++ PythonCode/MachineLearning.py
@@ -8,7 +8,27 @@ Created on Mon Dec 26 12:49:45 2022
 
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+from xgboost import XGBClassifier
+
+
+filename = 'model_xgb.pkl'
+
+rf_model = pickle.load(open(filename, 'rb'))
 
 def predict(education, joining_year, city, payment_tier, age, gender, ever_benched, experience):
-    #Usa gli otto input e poi ritorna leave_or_not, che sarà un boolean
-    pass
+    leave_or_not = None
+    input_values=np.array([education, joining_year, city, payment_tier, age, gender, ever_benched, experience], ndmin=2)
+    #input values non è un vettore, ma una matrice con una riga; è una distinzione importante per predict
+    output_value = rf_model.predict(input_values)
+    if output_value[0] == 1:
+        leave_or_not = True
+    else:
+        leave_or_not = False
+    #print(leave_or_not) #debug
+    return leave_or_not
+
+#predict(1, 2014, 3, 2, 37, 1, 0, 5) #debug
