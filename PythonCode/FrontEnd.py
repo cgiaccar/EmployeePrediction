18eecfc7fc7abf676990558ed36b63f7249d9325
Lorenzo Giaccari <lollo.jakk99@me.com>
2023-01-11 13:33:41 +0100

    First working frontend prototype for issue #21

--- PythonCode/FrontEnd.py
+++ PythonCode/FrontEnd.py
@@ -25,17 +25,17 @@ import streamlit as st
 # from tkinter.messagebox import showinfo
 # tkinter serve per listbox
 
-# import BackEnd as b
+import BackEnd as b
 
 # default values to avoid program crashing
-education = 1
-joining_year = 2012
-city = 1
-payment_tier = 1
-age = 18
-gender = 0
-ever_benched = 0
-experience = 0
+# education = 1
+# joining_year = 2012
+# city = 1
+# payment_tier = 1
+# age = 18
+# gender = 0
+# ever_benched = 0
+# experience = 0
 
 st.title("EmployeePrediction!!!")
 
@@ -53,6 +53,14 @@ if 'progress' not in st.session_state:
     # 7. PaymentTier
     # 8. EverBenched
     st.session_state['progress'] = 0
+    st.session_state['education'] = 1
+    st.session_state['joining_year'] = 2012
+    st.session_state['city'] = 1
+    st.session_state['payment_tier'] = 1
+    st.session_state['age'] = 18
+    st.session_state['gender'] = 0
+    st.session_state['ever_benched'] = 0
+    st.session_state['experience'] = 0
 
 if st.session_state['progress'] == 0:
     begin = st.button("Begin!")
@@ -61,51 +69,100 @@ if st.session_state['progress'] == 0:
             st.session_state['progress'] = 1
 
 if st.session_state['progress'] == 1:
-    # age question
-    # age = result
-    pass
+    possible_ages = []
+    for i in range(0, 83):
+        possible_ages.append(i+18)
+    st.text('Qual''è la tua età?')
+    select_age = st.selectbox('Seleziona Età', possible_ages)
+    st.write('Hai selezionato ', select_age)
+    result = select_age
+    confirm = st.button('Next', 1) #1 è la key del bottone, che altrimenti
+    #verrebbe generata in base al contenuto.
+    if confirm and result >= 18:
+        st.session_state['age'] = result
+        st.session_state['progress'] += 1
 
 if st.session_state['progress'] == 2:
-    # gender question
-    # gender = b.transform_gender(result)
-    pass
+    select_gender = st.selectbox('Seleziona Genere', ('Male', 'Female'))
+    st.write('Hai selezionato ', select_gender)
+    result = select_gender
+    confirm = st.button('Next', 2)
+    if confirm:
+        st.session_state['gender'] = b.transform_gender(result)
+        st.session_state['progress'] += 1
 
 if st.session_state['progress'] == 3:
-    # city question
-    # city = b.transform_city(result)
-    pass
+    select_city = st.selectbox('Seleziona Città', ('Bangalore', 'New Delhi', 'Pune'))
+    st.write('Hai selezionato ', select_city)
+    result = select_city
+    confirm = st.button('Next', 3)
+    if confirm:
+        st.session_state['city'] = b.transform_city(result)
+        st.session_state['progress'] += 1
 
 if st.session_state['progress'] == 4:
-    # education question
-    # education = b.transform_education(result)
-    pass
+    select_education = st.selectbox('Seleziona Educazione', ('Bachelor', 'Master', 'PHD'))
+    st.write('Hai selezionato ', select_education)
+    result = select_education
+    confirm = st.button('Next', 4)
+    if confirm:
+        st.session_state['education'] = b.transform_education(result)
+        st.session_state['progress'] += 1
 
 if st.session_state['progress'] == 5:
-    # joiningyear question
-    # joining_year = result
-    pass
+    possible_years = []
+    for i in range(0, 14):
+        possible_years.append(i+2010)
+    st.text('In che anno sei entrato a far parte dell''azienda?')
+    select_joining_year = st.selectbox('Seleziona Anno', possible_years)
+    st.write('Hai selezionato ', select_joining_year)
+    result = select_joining_year
+    confirm = st.button('Next', 5)
+    if confirm:
+        st.session_state['joining_year'] = result
+        st.session_state['progress'] += 1    
 
 if st.session_state['progress'] == 6:
-    # experience question
-    # experience = result
-    pass
+    possible_experience = []
+    for i in range(0, 8):
+        possible_experience.append(i)
+    st.text('Qual''è il tuo livello di esperienza?')
+    select_experience = st.selectbox('Seleziona Esperienza', possible_experience)
+    st.write('Hai selezionato ', select_experience)
+    result = select_experience
+    confirm = st.button('Next', 6)
+    if confirm:
+        st.session_state['experience'] = result
+        st.session_state['progress'] += 1
 
 if st.session_state['progress'] == 7:
-    # payment question
-    # payment_tier = b.transform_payment_tier(result)
-    pass
+    select_payment_tier = st.selectbox('Seleziona Tier di Pagamento', (1, 2, 3))
+    st.write('Hai selezionato ', select_payment_tier)
+    result = select_payment_tier
+    confirm = st.button('Next', 7)
+    if confirm:
+        st.session_state['payment_tier'] = b.transform_payment_tier(result)
+        st.session_state['progress'] += 1
 
 if st.session_state['progress'] == 8:
-    # everbenched question
-    # ever_benched = b.transform_ever_benched(result)
-    pass
+    st.text('Sei mai stato messo in panchina da un progetto per più di un mese?')
+    select_ever_benched = st.selectbox('Seleziona Risposta', ('No', 'Yes'))
+    st.write('Hai selezionato ', select_ever_benched)
+    result = select_ever_benched
+    confirm = st.button('Next', 8)
+    if confirm:
+        st.session_state['ever_benched'] = b.transform_ever_benched(result)
+        st.session_state['progress'] += 1
 
 if st.session_state['progress'] == 9:
-    # leave_or_not = b.predict(education, joining_year, city, payment_tier,
-    #   age, gender, ever_benched, experience)
-    # if leave_or_not:
-    # ...
-    # else:
-    # ...
-    # modo per tornare a session_state['progress'] = 0
-    pass
+    st.write(st.session_state['education'], st.session_state['joining_year'], st.session_state['city'], st.session_state['payment_tier'],
+                        st.session_state['age'], st.session_state['gender'], st.session_state['ever_benched'], st.session_state['experience']) #debug
+    leave_or_not = b.predict(st.session_state['education'], st.session_state['joining_year'], st.session_state['city'], st.session_state['payment_tier'],
+                        st.session_state['age'], st.session_state['gender'], st.session_state['ever_benched'], st.session_state['experience'])
+    if leave_or_not:
+        st.text('Entro due anni, l''impiegato lascerà l''azienda!!!')
+    else:
+        st.text('Probabilmente, l''impiegato non lascerà l''azienda nei prossimi due anni.')
+    back_button = st.button("Back to Homepage")
+    if back_button:
+        st.session_state['progress'] = 0
